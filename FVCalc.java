e29d3d4883847cd6a9ea4587bd3318f81b2fcd8e
Gabrielle Geffen <gabrielle.geffen@post.runi.ac.il>
2025-11-06 12:14:56 +0200

    Completed assignment

--- FVCalc.java
+++ FVCalc.java
@@ -2,10 +2,12 @@
 public class FVCalc {
 	public static void main(String[] args){
 		int currentValue = Integer.parseInt(args[0]);
-		double rate = Double.parseDouble(args[1]);
+		double rate = Double.parseDouble(args[1])/100;
 		int n = Integer.parseInt(args[2]);
 		double futureValue = currentValue * Math.pow(rate+1,n);
 
-		System.out.println((int) futureValue);
+		System.out.println("After "+ n + " years, $" + currentValue + " saved at " + rate*100 + "% will yield $" + (int) futureValue);
+			//After 2 years, $100 saved at 10.0% will yield $121
+
 	}
 }
